7f15d4ebc2515d794a5878b8c4de8439c9bf4d92
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2021-08-14 14:36:41 +1200

    Changes 14/08/2021

---
+++ 1.3/Source/MorrowRim_Orsimir/MorrowRim_Orsimir/Harmony.cs
@@ -0,0 +1,61 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using System.Reflection;
+using Verse;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Verse.AI;
+using Verse.AI.Group;
+using System.Text;
+using UnityEngine;
+
+namespace MorrowRim_Orsimir
+{
+    //Setting the Harmony instance
+    [StaticConstructorOnStartup]
+    public class Main
+    {
+        static Main()
+        {
+            var harmony = new Harmony("com.MorrowRim_Orsimir");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    /* 
+     * Patch that checks for thing made of orichalcum and made by Orsimer
+     * If thing is found, increase quality level by 1
+     */
+
+    [HarmonyPatch(typeof(QuestManager))]
+    [HarmonyPatch("Notify_ThingsProduced")]
+    public static class QuestManager_NotifyThingsProduced_Patch
+    {
+        [HarmonyPostfix]
+        [HarmonyPriority(Priority.Last)]
+        public static void CheckOrichalcum(Pawn worker, List<Thing> things)
+        {
+            Log.Message("1");
+            foreach (Thing thing in things)
+            {
+                Log.Message(": " + thing);
+                if (thing.def.CostStuffCount > 0 && HarmonyUtility.MadeOfOrichalc(thing) && HarmonyUtility.IsOrsimer(worker))
+                {
+                    Log.Message("2");
+                    if (thing.def.comps.Any(x => x.compClass.Name == "CompQuality"))
+                    {
+                        Log.Message("3");
+                        thing.TryGetQuality(out QualityCategory qc);
+                        if (qc != QualityCategory.Legendary)
+                        {
+                            thing.TryGetComp<CompQuality>().SetQuality(qc + 1, new ArtGenerationContext());
+                            Log.Message("Quality increased for: " + thing + ", from: " + qc);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 1.3/Source/MorrowRim_Orsimir/MorrowRim_Orsimir/HarmonyUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using System.Reflection.Emit;
+using System.Linq;
+
+namespace MorrowRim_Orsimir
+{
+    public static class HarmonyUtility
+    {
+        /* For qulity patch */
+        public static bool IsOrsimer(Pawn p)
+        {
+            Log.Message("1.1");
+            return p != null && p.def == ThingDef.Named("MorrowRim_OrsimerRace");
+        }
+
+        public static bool MadeOfOrichalc(Thing t)
+        {
+            Log.Message("1.2");
+            return t != null && t.Stuff != null && t.Stuff == ThingDef.Named("MorrowRim_Orichalcum");
+        }
+    }
+}
